c479804b5051b966bf542cb9c3b26c59ac2fa31d
benzlxs <benzlee08@gmail.com>
2023-05-19 21:34:39 +1000

    add crop

--- make_video.py
+++ make_video.py
@@ -8,10 +8,15 @@ import cv2 as cv
 from moviepy.editor import ImageSequenceClip
 
 
+def making_video(args, file_list):
+    file_list.sort()
+    clip = ImageSequenceClip(file_list, fps=args.fps)
+    clip.write_videofile( 'full_' + str(args.fps) + ".mp4",fps=args.fps)
+
 def main(args):
     if args.resize == True:
         print("resize image")
-        original_dir_images = os.path.join(args.image_path f"image")
+        original_dir_images = os.path.join(args.image_path, f"image")
         original_images = glob.glob(original_dir_images + '/*')
         dir_images = os.path.join(args.image_pathe, f"image_re")
         os.makedirs(dir_images, exist_ok=True)
@@ -23,13 +28,27 @@ def main(args):
             img_resize = cv.resize(img_rgb, (wid, hei), interpolation = cv.INTER_AREA)
             cv.imwrite(os.path.join(dir_images, img_name), img_resize)
 
-        file_list = glob.glob(os.path.join(dir_images,'*.png'))
+        file_list = glob.glob(os.path.join(dir_images,'*.'))
     else:
-        file_list = glob.glob(os.path.join(args.image_path, '*.png'))
-    file_list.sort()
-    clip = ImageSequenceClip(file_list, fps=15)
-    name = '15.gif'
-    clip.write_videofile("15.mp4",fps=15)
+        file_list = glob.glob(os.path.join(args.image_path, '*.'))
+
+    making_video(args, file_list)
+
+    if args.crop:
+        print("cropping image")
+        original_dir_images = os.path.join(args.image_path, f"image_re")
+        original_images = glob.glob(original_dir_images + '/*')
+        dir_images = os.path.join(args.image_pathe, f"image_crop")
+        os.makedirs(dir_images, exist_ok=True)
+        s_p = args.start_point
+        e_p = args.end_point
+        for o_img_dir in original_images:
+            img_rgb  = cv.imread(o_img_dir)
+            img_name = o_img_dir.split('/')[-1]
+            img_crop = img_rgb[s_p[0]:e_p[0], s_p[1], e_p[1], :]
+            cv.imwrite(os.path.join(dir_images, img_name), img_crop)
+        file_list = glob.glob(os.path.join(dir_images, '*'))
+        making_video(args, file_list)
 
 if __name__ == "__main__":
     # Parser

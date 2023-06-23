a225787886cea4d3b48f193225abd14362e756f6
Xuesong Li <li325@shazza.noreply.csiro.au>
2023-06-23 17:43:40 +1000

    sim funct

--- similarity/img_similarity.py
+++ similarity/img_similarity.py
@@ -18,21 +18,35 @@ import math
 from argparse import ArgumentParser
 from math import exp, sqrt
 
+
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
+    return gauss / gauss.sum()
+
+
+def create_window(window_size, channel, std=1.5):
+    _1D_window = gaussian(window_size, std).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).unsqueeze(0).unsqueeze(0)
+    window = _2D_window.expand(channel, 1, window_size, window_size).contiguous()
+    return window
+
+
 def add_noise_func(img_input, noise_type='gaussian', save=True):
+    print("Adding noise of", noise_type)
     img = np.copy(img_input)
-    height, width = img.shape
+    height, width, channel = img.shape
     if noise_type == 'gaussian':
         mean = 0.1
         std = 0.1
         noise = np.random.normal(mean, std, img.shape).astype(np.float32)
-        noisy_img = img + noise
+        noisy_img = img + noise*255.
     elif noise_type == 'slat':
         probability = 0.1
         num_pixels = int(probability * height * width)
         indices = np.random.choice(range(height * width), num_pixels, replace=False)
-        img = img.reshape(-1)
-        img[indices] = 0.3
-        noisy_img = img.reshape(height, width)
+        img = img.reshape(-1, channel)
+        img[indices,:] = 0.3 * 255.
+        noisy_img = img.reshape(height, width, channel)
     elif noise_type == 'possion':
         noisy_img = np.random.poisson(img)
     elif noise_type == 'quantization':
@@ -45,7 +59,6 @@ def add_noise_func(img_input, noise_type='gaussian', save=True):
         cv.imwrite('raw_gray.png', img_input * 255.)
         cv.imwrite('noisy_gray.png', noisy_img * 255.)
     return noisy_img
-
 
 
 def function_ncc(ref_gray, src_grays):
@@ -89,6 +102,27 @@ def function_ncc(ref_gray, src_grays):
     return ncc
 
 
+def function_ssim(pred, gt, window, channel):
+    ntotpx, nviews, nc, h, w = pred.shape
+    flat_pred = pred.view(-1, nc, h, w)
+    mu1 = F.conv2d(flat_pred, window, padding=0, groups=channel).view(ntotpx, nviews, nc)
+    mu2 = F.conv2d(gt, window, padding=0, groups=channel).view(ntotpx, nc)
+
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2).unsqueeze(1)
+    mu1_mu2 = mu1 * mu2.unsqueeze(1)
+
+    sigma1_sq = F.conv2d(flat_pred * flat_pred, window, padding=0, groups=channel).view(ntotpx, nviews, nc) - mu1_sq
+    sigma2_sq = F.conv2d(gt * gt, window, padding=0, groups=channel).view(ntotpx, 1, 3) - mu2_sq
+    sigma12 = F.conv2d((pred * gt.unsqueeze(1)).view(-1, nc, h, w), window, padding=0, groups=channel).view(ntotpx, nviews, nc) - mu1_mu2
+
+    C1 = 0.01 ** 2
+    C2 = 0.03 ** 2
+
+    values = 1 - ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+    return torch.sum(values, dim=2) / 2
+
+
 def main(args):
     img_0 = cv.imread(args.img0_dir)
     print("img 0 size:", img_0.shape)
@@ -99,31 +133,42 @@ def main(args):
     img_0 = cv.resize(img_0, [fix_hw, fix_hw])
     img_1 = cv.resize(img_1, [fix_hw, fix_hw])
 
-    img_0_gray = cv.cvtColor(img_0, cv.COLOR_BGR2GRAY).astype(np.float32)/255.0
-    img_1_gray = cv.cvtColor(img_1, cv.COLOR_BGR2GRAY).astype(np.float32)/255.0
+    if args.with_noise:
+        img_1 = add_noise_func(img_1, noise_type='gaussian')
 
-    with_noise = True
-    if with_noise:
-        img_1_gray = add_noise_func(img_1_gray, noise_type='gaussian')
-
-    ref_gray = img_0_gray.reshape(1,1,-1,1)
-    ref_gray = torch.from_numpy(ref_gray)
-    src_gray = img_1_gray.reshape(1,1,-1,1)
-    src_gray = torch.from_numpy(src_gray)
-
+    print("The similarity metric is:", args.similarity_metric)
     if args.similarity_metric == "NCC":
+        img_0_gray = cv.cvtColor(img_0, cv.COLOR_BGR2GRAY).astype(np.float32)/255.0
+        img_1_gray = cv.cvtColor(img_1, cv.COLOR_BGR2GRAY).astype(np.float32)/255.0
+
+        ref_gray = img_0_gray.reshape(1,1,-1,1)
+        ref_gray = torch.from_numpy(ref_gray)
+        src_gray = img_1_gray.reshape(1,1,-1,1)
+        src_gray = torch.from_numpy(src_gray)
+
         similarity = function_ncc(ref_gray, src_gray)
-        print("The similarity between two patches:", similarity)
     elif args.similarity_metric == "SSIM":
-        similarity = function_ssim(img_0_gray, img_1_gray)
+        channels = 3
+        img_pred = torch.from_numpy(img_0)
+        img_gt   = torch.from_numpy(img_1)
+        img_pred = img_pred[None,None,:,:,:]
+        img_pred = img_pred.permute(0, 1, 4, 2, 3).contiguous() / 255.
+        img_gt   = img_gt[None,:,:,:]
+        img_gt   = img_gt.permute(0, 3, 1, 2) / 255.
+        window_filter = create_window(fix_hw, channels)
+        similarity = function_ssim(img_pred, img_gt, window_filter, channels)
     else:
         raise NotImplementedError("Other methods have been implimented!")
+
+    print("The similarity between two patches:", similarity)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--img0_dir", type=str, default="000026_sqrt.png", help="image 0 directory")
     parser.add_argument("--img1_dir", type=str, default="000001_sqrt.png", help="image 1 directory")
     parser.add_argument("--similarity_metric", type=str, default="NCC")
+    parser.add_argument("--with_noise", action="store_true",
+                        default=False, help= "Displaying the inside colors")
     args = parser.parse_args()
     main(args)
 
